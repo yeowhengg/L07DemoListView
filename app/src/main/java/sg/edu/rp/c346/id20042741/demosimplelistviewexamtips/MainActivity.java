cc52c4d62811151eb1f29f964f378c8c47f3f933
Yeow Heng <20042741@myrp.edu.sg>
2022-06-03 11:35:52 +0800

    using arraylist instead of a static array

--- app/src/main/java/sg/edu/rp/c346/id20042741/demosimplelistviewexamtips/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20042741/demosimplelistviewexamtips/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     ListView lvExamTips;
     String[] examTipsArray;
+    ArrayList<String> examTipsAL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,11 +19,21 @@ public class MainActivity extends AppCompatActivity {
 
         lvExamTips = findViewById(R.id.listViewExamTips); //get the listview
 
-        // an array to store values
-        examTipsArray = new String[]{"Don't just read the code, code it as much as possible during each practical session", "Seek help from the lecturer ASAP, don't wait till you lost in the jungle", "Prepare your template source code for each topic", "Create a few empty Android projects to speed up your coding during the Exam", "Ensure that your Android Studio is up and running before the exam"};
+        examTipsAL = new ArrayList<String>();
 
-        // an arrayadapter is to adapting an array of objects as a datasource
-        ArrayAdapter examTipsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, examTipsArray);
+        examTipsAL.add("Don't just read the code, code it as much as possible during each practical session");
+        examTipsAL.add("Seek help from the lecturer ASAP, don't wait till you lost in the jungle");
+        examTipsAL.add("Prepare your template source code for each topic");
+        examTipsAL.add("Create a few empty Android projects to speed up your coding during the Exam");
+        examTipsAL.add("Ensure that your Android Studio is up and running before the exam");
+
+
+
+        // an array to store values
+        //examTipsArray = new String[]{"Don't just read the code, code it as much as possible during each practical session", "Seek help from the lecturer ASAP, don't wait till you lost in the jungle", "Prepare your template source code for each topic", "Create a few empty Android projects to speed up your coding during the Exam", "Ensure that your Android Studio is up and running before the exam"};
+
+        // an arrayadapter is to adapt an array of objects as a datasource
+        ArrayAdapter examTipsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, examTipsAL);
 
         // populates the listview with the datasource value in the adapter;
         lvExamTips.setAdapter(examTipsAdapter);
